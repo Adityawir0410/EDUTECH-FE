fa484d8a6b6053ae4c2725f8e0387737bcc1f1b3
Aditya Wirayudha <83379121+Adityawir0410@users.noreply.github.com>
2024-12-08 21:10:01 +0700

    coba 2

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -3,24 +3,24 @@
 import React, { useState } from "react";
 // import Login from "../screens/auth/login";
 // import Register from "../screens/auth/register";
-import Home from "../screens/home/home";
+// import Home from "../screens/home/home";
 import LandingPage from "../screens/landingpage/landingpage";
 
 const Routes: React.FC = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [currentPage, setCurrentPage] = useState("landing"); // 'landing', 'login', or 'register'
 
-  const handleLoginSuccess = () => {
-    setIsAuthenticated(true);
-  };
+  // const handleLoginSuccess = () => {
+  //   setIsAuthenticated(true);
+  // };
 
   return (
     <div>
       {currentPage === "landing" ? (
         <LandingPage />
       ) : isAuthenticated ? (
-        <Home />
-      ) : currentPage === "login" ? (
+      //   <Home />
+      // ) : currentPage === "login" ? (
         // <Login
         //   onLoginSuccess={handleLoginSuccess}
         //   onRegister={() => setCurrentPage("register")}
--- src/app/screens/home/home.tsx
+++ src/app/screens/home/home.tsx
@@ -1,13 +1,13 @@
-"use client";
+// "use client";
 
-import React from 'react';
+// import React from 'react';
 
-const Home: React.FC = () => {
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800">Welcome</h1>
-    </div>
-  );
-};
+// const Home: React.FC = () => {
+//   return (
+//     <div className="flex justify-center items-center min-h-screen bg-gray-100">
+//       <h1 className="text-4xl font-bold text-gray-800">Welcome</h1>
+//     </div>
+//   );
+// };
 
-export default Home;
+// export default Home;
