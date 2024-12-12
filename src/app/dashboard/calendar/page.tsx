c83cdc9df91afa50ab5d0bd597b29d64fc995e44
Aditya Wirayudha <83379121+Adityawir0410@users.noreply.github.com>
2024-12-12 18:57:22 +0700

    terbaru

---
+++ src/app/dashboard/calendar/page.tsx
@@ -0,0 +1,19 @@
+// "use client";
+
+// import React from "react";
+// import CardVideo from "@/app/dashboard/component/cardvideo";
+
+// const ClassesPage: React.FC = () => {
+//   return (
+//     <div className="flex flex-col min-h-screen p-6">
+
+//       {/* Container untuk CardVideo */}
+//       <div className="flex flex-wrap gap-6">
+//         {/* Contoh beberapa CardVideo */}
+//         <CardVideo />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ClassesPage;
