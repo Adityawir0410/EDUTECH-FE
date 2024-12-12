b402e071d34af2c3660499c64cd9120c04759050
Aditya Wirayudha <83379121+Adityawir0410@users.noreply.github.com>
2024-12-12 19:02:11 +0700

    tanpa comand

--- src/app/dashboard/calendar/page.tsx
+++ src/app/dashboard/calendar/page.tsx
@@ -1,19 +1,19 @@
-// "use client";
+"use client";
 
-// import React from "react";
-// import CardVideo from "@/app/dashboard/component/cardvideo";
+import React from "react";
+import CardVideo from "@/app/dashboard/component/cardvideo";
 
-// const ClassesPage: React.FC = () => {
-//   return (
-//     <div className="flex flex-col min-h-screen p-6">
+const ClassesPage: React.FC = () => {
+  return (
+    <div className="flex flex-col min-h-screen p-6">
 
-//       {/* Container untuk CardVideo */}
-//       <div className="flex flex-wrap gap-6">
-//         {/* Contoh beberapa CardVideo */}
-//         <CardVideo />
-//       </div>
-//     </div>
-//   );
-// };
+      {/* Container untuk CardVideo */}
+      <div className="flex flex-wrap gap-6">
+        {/* Contoh beberapa CardVideo */}
+        <CardVideo />
+      </div>
+    </div>
+  );
+};
 
-// export default ClassesPage;
+export default ClassesPage;
